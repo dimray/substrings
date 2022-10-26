45d744101044fe59c538335361e38b5533c37c54
dimray <dimray@gmail.com>
2022-10-26 17:51:04 +0100

    clarify variable name

--- sub-strings.rb
+++ sub-strings.rb
@@ -3,9 +3,9 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(str, arr)
   results = Hash.new
   array_from_string = str.split(" ")
-  array_from_string.each do |str|  
+  array_from_string.each do |word_from_string|  
     arr.each do |word|
-      if str.downcase.include?(word)
+      if word_from_string.downcase.include?(word)
         if results.include?(word)
           results[word] += 1
         else 
